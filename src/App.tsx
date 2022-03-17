6786fc1423d9bce3c7197a7f4f694f6d006510fb
youngvvp <andreyrussia111@gmail.com>
2022-03-17 16:47:06 +0300

    lesson 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {TodoList} from "./TodoList";
-
+import {Todolist} from './Todolist';
 
 function App() {
 
-    const task1 = [
-        {id: 1, title: "HTMLSS", isDone: true},
+    /* let tasks1 = [
+         { id: 1, title: "HTML&CSS", isDone: true },
+         { id: 2, title: "JS", isDone: true },
+         { id: 3, title: "ReactJS", isDone: false }
+     ]*/
+
+    let [tasks1, setTasks1] = useState([
+        {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false}
-    ];
+    ])
 
-    const task2 = [
-        {id: 1, title: "Hello Wordl", isDone: true},
-        {id: 2, title: "I am happy", isDone: false},
-        {id: 3, title: "Yo", isDone: false}
-    ];
+    const removeTask = (id: number) => {
+        let result = tasks1.filter(item => {
+            return item.id !== id
+        })
+        setTasks1(result)
+    }
 
+/*    let [valueButton, setValueButton] = useState('all');
+
+    const tasksFilter = (nameButton: string) => {
+        setValueButton(nameButton)
+    }
+
+    let prokladka = tasks1;
+
+    if (valueButton === 'active') {
+        prokladka = tasks1.filter(el => !el.isDone)
+    }
+    if (valueButton === 'completed') {
+        prokladka = tasks1.filter(el => el.isDone)
+    }*/
 
     return (
         <div className="App">
-            <TodoList title={'Zagolovok 1'} task={task1}/>
-            <TodoList title={'Zagolovok 2'} task={task2}/>
+            <Todolist title="What to learn"
+                      tasks={tasks1}
+                      removeTask={removeTask}
+                      /*tasksFilter={tasksFilter}*//>
         </div>
     );
 }
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,39 +1,57 @@
-import React from "react";
+import React, {useState} from 'react';
 
-type TodoListPropsType = {
-    title: string,
-    task: Array<TaskPropsType>
-}
-
-type TaskPropsType = {
-    id: number,
-    title: string,
+type TaskType = {
+    id: number
+    title: string
     isDone: boolean
 }
 
-export const TodoList = (props: TodoListPropsType) => {
-    return (
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
+    removeTask: (id: number) => void
+    /*tasksFilter: (nameButton: 'all' | 'active' | 'completed') => void*/
+}
+
+export function Todolist(props: PropsType) {
+
+    let [valueButton, setValueButton] = useState('all');
+
+    const tasksFilter = (nameButton: string) => {
+        setValueButton(nameButton)
+    }
+
+    let prokladka = props.tasks;
+
+    if (valueButton === 'active') {
+        prokladka = props.tasks.filter(el => !el.isDone)
+    }
+    if (valueButton === 'completed') {
+        prokladka = props.tasks.filter(el => el.isDone)
+    }
+
+    return <div>
+        <h3>{props.title}</h3>
         <div>
-            <h3>{props.title}</h3>
-            <div>
-                <input/>
-                <button>+</button>
-            </div>
-            <ul>
-                {props.task.map( (el: TaskPropsType) => {
-                    return(
-                        <li><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>
-                    )
-                })}
-               {/* <li><input type="checkbox" checked={props.task[0].isDone}/> <span>{props.task[0].title}</span></li>
-                <li><input type="checkbox" checked={props.task[1].isDone}/> <span>{props.task[1].title}</span></li>
-                <li><input type="checkbox" checked={props.task[2].isDone}/> <span>{props.task[2].title}</span></li>*/}
-            </ul>
-            <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
-            </div>
+            <input/>
+            <button>+</button>
         </div>
-    )
+        <ul>
+            {
+                prokladka.map((el) => {
+                    return (
+                        <li key={el.id}>
+                            <button onClick={ () => props.removeTask(el.id) }>x</button>
+                            <input type="checkbox" checked={el.isDone}/> <span>{el.title}</span>
+                        </li>
+                    )
+                })
+            }
+        </ul>
+        <div>
+            <button onClick={ () => tasksFilter('all')}>All</button>
+            <button onClick={ () => tasksFilter('active')}>Active</button>
+            <button onClick={ () => tasksFilter('completed')}>Completed</button>
+        </div>
+    </div>
 }
